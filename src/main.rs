e4377cd51f18b208f26139682d9f7eff67787733
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-13 21:59:57 +0500

    program showing how to declare and use Array in RUST

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // declaring with array size and type
+    let marks:[i32;5] = [83,64,91,78,88];
+    // printing complete array
+    println!("{:?}", marks);
+    // indexing through array
+    println!("{}", marks[2]);
+    println!("{}", marks[4]);
+}
